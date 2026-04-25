8fe0edb04093a530a9baa9b7db329a41496dcc45
Bogyi95 <krzysiek.bogusz@onet.pl>
2026-04-25 16:34:02 +0200

    auto detect docker compose version

--- start.sh
+++ start.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
-if [$# -lt 1]; then
-    echo "Usage: ./start.sh <PORT>"
-    exit 1
+if [ $# -lt 1 ]; then
+  echo "Usage: ./start.sh <PORT>"
+  exit 1
 fi
 
 PORT=$1
 
-PORT=$PORT docker compose up --build -d 
-echo "Aplikacja dziala na http://localhost:${PORT}"
+if docker compose version > /dev/null 2>&1; then
+  PORT=$PORT docker compose up --build -d
+else
+  PORT=$PORT docker-compose up --build -d
+fi
+
+echo "App is running at http://localhost:${PORT}"
